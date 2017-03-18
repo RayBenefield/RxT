aafe001e0658a980f60c0e3f82938361d989cdbf
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-03-18 12:10:33 -0700

    Changed the system to have a full spec instead of individual tests.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 /* eslint-disable no-console */
 import _ from 'lodash';
+import clivas from 'clivas';
 import { Observable } from 'rxjs';
 import createExample from './example-observable';
 
-const toTapFormat = (ex) => {
-    if (ex.result === 'pass') return `ok - ${ex.description}`;
-    return `not ok - ${ex.description}
-  ---
-  message: ${JSON.stringify(ex.error._message)}
-  severity: fail
-  ...`;
-};
-
 _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
-console.log('TAP version 13');
-export default (description, specification) => {
-    const example = specification(createExample(description));
-    console.log('1..1');
-    Observable.of(0)
-        .mergeMap(() => example
+export default (specDescription, specCreator) => {
+    const examples = [];
+    specCreator((description, specification) => {
+        const example = specification(createExample(description));
+        examples.push(example
             .extend({ result: 'pass' })
             .catch((error) => {
                 if (error.name !== 'AssertionError') return Observable.of(error);
@@ -26,7 +17,14 @@ export default (description, specification) => {
                     _.extend(error.example, { result: 'fail', error }),
                 );
             })
-        )
-        .map(toTapFormat)
-        .subscribe(console.log);
+        );
+    });
+
+    Observable.merge(...examples)
+        .scan((all, current) => [].concat(all, [current]), [])
+        .subscribe((result) => {
+            clivas.clear();
+            result.forEach(item => clivas.line(item.description));
+        });
 };
+
--- src/index.test.js
+++ src/index.test.js
@@ -3,54 +3,56 @@ import 'should';
 import capitalized from 'lodash/capitalize';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
-import it from '.';
+import describe from '.';
 
 const observableCapitalized = given => Observable.of(capitalized(given));
 
-it('should capitalize just hello', ex => ex
-    .given('hello')
-    .when(capitalized)
-    .then(actual => actual.should.be.exactly('Hello'))
-);
+describe('Testing', (it) => {
+    it('should capitalize just hello', ex => ex
+        .given('hello')
+        .when(capitalized)
+        .then(actual => actual.should.be.exactly('Hello'))
+    );
 
-it('should capitalize {{given}}', ex => ex
-    .givenEach(
-        ['hello', 'world']
-    )
-    .when(capitalized)
-    .thenEach(
-        (actual, expected) => {
-            actual.length.should.be.exactly(5);
-            actual.should.be.exactly(expected);
-        },
-        ['Hello', 'World']
-    )
-);
+    it('should capitalize {{given}}', ex => ex
+        .givenEach(
+            ['hello', 'world']
+        )
+        .when(capitalized)
+        .thenEach(
+            (actual, expected) => {
+                actual.length.should.be.exactly(5);
+                actual.should.be.exactly(expected);
+            },
+            ['Hello', 'World']
+        )
+    );
 
-it('should capitalize {{given}} with an observable', ex => ex
-    .givenEach(
-        ['hello', 'world']
-    )
-    .whenObserving(observableCapitalized)
-    .thenEach(
-        (actual, expected) => {
-            actual.length.should.be.exactly(5);
-            actual.should.be.exactly(expected);
-        },
-        ['Hello', 'World']
-    )
-);
+    it('should capitalize {{given}} with an observable', ex => ex
+        .givenEach(
+            ['hello', 'world']
+        )
+        .whenObserving(observableCapitalized)
+        .thenEach(
+            (actual, expected) => {
+                actual.length.should.be.exactly(5);
+                actual.should.be.exactly(expected);
+            },
+            ['Hello', 'World']
+        )
+    );
 
-it('should fail {{given}} with an observable', ex => ex
-    .givenEach(
-        ['hello', 'world']
-    )
-    .whenObserving(observableCapitalized)
-    .thenEach(
-        (actual, expected) => {
-            actual.length.should.be.exactly(5);
-            actual.should.be.exactly(expected);
-        },
-        ['hello', 'world']
-    )
-);
+    it('should fail {{given}} with an observable', ex => ex
+        .givenEach(
+            ['hello', 'world']
+        )
+        .whenObserving(observableCapitalized)
+        .thenEach(
+            (actual, expected) => {
+                actual.length.should.be.exactly(5);
+                actual.should.be.exactly(expected);
+            },
+            ['hello', 'world']
+        )
+    );
+});
