1fcba2d985820c9b3354d6b89a36c253fd3e4ccd
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-03-18 08:21:41 -0700

    Added initial tap formatter.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import _ from 'lodash';
 import { Observable } from 'rxjs';
 import createExample from './example-observable';
 
+const toTapFormat = (ex) => {
+    if (ex.result === 'pass') return 'ok';
+    return 'not ok';
+};
+
 _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 export default (description, specification) => {
     const example = specification(createExample(description));
+    console.log('TAP version 13');
+    console.log('1..5');
     Observable.of(0)
         .mergeMap(() => example
             .extend({ result: 'pass' })
@@ -16,6 +23,7 @@ export default (description, specification) => {
                 );
             })
         )
-        .subscribe(ex => console.log(ex));
+        .map(toTapFormat)
+        .subscribe(console.log);
 };
 
