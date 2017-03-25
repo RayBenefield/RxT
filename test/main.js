a7c21fc87578062b393e897f3b3ca5ff4c8be542
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-03-25 09:01:40 -0700

    Test the actual test result.

--- test/main.js
+++ test/main.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console, no-duplicate-imports, import/no-duplicates */
+import _ from 'lodash';
 import proxyquire from 'proxyquire';
 import describe from '../src';
 import { specStream } from '../src';
@@ -17,13 +18,18 @@ describe('RxT', (it) => {
         .then(
             (result) => {
                 const expected = [
-                    ['should capitalize just hello'],
-                    [
-                        'should capitalize just hello',
-                        'should fail to assert the proper hello',
-                    ],
+                    {
+                        'should capitalize just hello': 'pass',
+                    },
+                    {
+                        'should capitalize just hello': 'pass',
+                        'should fail to assert the proper hello': 'fail',
+                    },
                 ];
-                result.results.should.have.keys(...expected[result.step]);
+                result.results.should.have.keys(..._.keys(expected[result.step]));
+                _.forIn(expected[result.step], (val, key) => {
+                    result.results[key].result.should.equal(val);
+                });
             },
         )
     );
