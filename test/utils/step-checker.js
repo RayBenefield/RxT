9bae2c59a5bb94bbd524e2c6a14f1238b09fa2d8
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-03-27 11:26:05 -0700

    Refactored out the stepAccumulator.

--- test/utils/step-checker.js
+++ test/utils/step-checker.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 
+export const stepAccumulator = timeline => timeline.reduce((past, current) => {
+    if (past.length === 0) return [current];
+    return [].concat(past, [_.assign({}, past[past.length - 1], current)]);
+}, []);
+
 export default module.exports = expected => (result) => {
-    const steps = expected.reduce((past, current) => {
-        if (past.length === 0) return [current];
-        return [].concat(past, [_.assign({}, past[past.length - 1], current)]);
-    }, []);
+    const steps = stepAccumulator(expected);
     result.results.should.have.keys(..._.keys(steps[result.step]));
     _.forIn(steps[result.step], (val, key) => {
         result.results[key].result.should.equal(val);
